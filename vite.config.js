5b203f8450a3e1ad30df63899d15204d92b9f77d
Musa Yazlık <musayazlik1997@gmail.com>
2025-05-10 00:12:02 +0300

    Refactor Navbar and related components for improved structure and performance
    - Updated Navbar component to streamline menu fetching and rendering.
    - Refactored NavbarLogo, NavbarMenu, and NavbarTop components for better readability and maintainability.
    - Enhanced ProductsPage and ProductsText components to improve data fetching and loading states.
    - Adjusted LanguageContext and LanguageSwitcher for better language management.
    - Cleaned up CSS styles in Tabs.css and index.css for consistency.
    - Modified Vite configuration to allow local development on localhost and 127.0.0.1.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['polkachemicals.com'],
+    allowedHosts: ["localhost", "127.0.0.1"],
   },
-})
+});

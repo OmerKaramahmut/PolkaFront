782bc328cb0e49f1d7c8fab630c8996b5da91646
Musa Yazlık <musayazlik1997@gmail.com>
2025-05-10 03:00:53 +0300

    Update .env file to include VITE_BASE_URL and refactor NavbarLogo component for improved logo URL handling

--- src/components/Navbar/NavbarLogo.jsx
+++ src/components/Navbar/NavbarLogo.jsx
@@ -1,34 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const NavbarLogo = () => {
-    const [logoUrl, setLogoUrl] = useState('');
+  const [logoUrl, setLogoUrl] = useState("");
 
-    useEffect(() => {
-        const fetchLogo = async () => {
-            try {
-                const res = await axios.get(`${API_BASE_URL}/navbar-logo?populate=logo`);
-                const url = res.data?.data?.logo?.url;
-                if (url) {
-                    setLogoUrl(`${API_BASE_URL}${url}`);
-                }
-            } catch (err) {
-                console.error("Logo alınamadı:", err);
-            }
-        };
+  useEffect(() => {
+    const fetchLogo = async () => {
+      try {
+        const res = await axios.get(
+          `${API_BASE_URL}/navbar-logo?populate=logo`
+        );
+        const url = res.data?.data?.logo?.url;
+        if (url) {
+          setLogoUrl(`${BASE_URL}${url}`);
+        }
+      } catch (err) {
+        console.error("Logo alınamadı:", err);
+      }
+    };
 
-        fetchLogo();
-    }, []);
+    fetchLogo();
+  }, []);
 
-    return (
-        <div>
-            <a className="navbar-brand" href="/">
-                {logoUrl && <img src={logoUrl} alt="Logo" style={{ height: '100px', width: "300px" }} />}
-            </a>
-        </div>
-    );
+  return (
+    <div>
+      <a className="navbar-brand" href="/">
+        {logoUrl && (
+          <img
+            src={logoUrl}
+            alt="Logo"
+            style={{ height: "100px", width: "300px" }}
+          />
+        )}
+      </a>
+    </div>
+  );
 };
 
 export default NavbarLogo;
